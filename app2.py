626d8f60047dabac5e2b14eed1a047e2935c3680
mihaimodan <78228595+mihaimodan@users.noreply.github.com>
2021-02-16 14:24:02 +0200

    Added marker.

--- app2.py
+++ app2.py
@@ -1,4 +1,8 @@
 import folium
 
-map = folium.Map(location = [44.719351, 24.9279223], zoom_start = 6) #Current location when writing this
- 
+map = folium.Map(location = [44.719351, 24.9279223], zoom_start = 6, tiles = "OpenStreetMap" ) #Current location when writing this
+
+#Personalizing the map object with .add_child.
+map.add_child(folium.Marker(location = [44.7, 24.9], popup ="Peek-a-boo!", icon =folium.Icon(color = "blue")))
+
+map.save("Map test.html")
