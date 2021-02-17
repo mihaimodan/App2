a712314e8e9f34a9f23f78002c353d6e3e7eeebb
mihaimodan <78228595+mihaimodan@users.noreply.github.com>
2021-02-17 14:35:39 +0200

    Added input file Volcanoes.txt as data frames. Addes a for loop to iterate through lat and long.

--- app2.py
+++ app2.py
@@ -1,8 +1,20 @@
 import folium
+import pandas as pd
 
-map = folium.Map(location = [44.719351, 24.9279223], zoom_start = 6, tiles = "OpenStreetMap" ) #Current location when writing this
+data = pd.read_csv("Volcanoes.txt")
+lat = data["LAT"].to_list()
+lon = data["LON"].to_list()
 
-#Personalizing the map object with .add_child.
-map.add_child(folium.Marker(location = [44.7, 24.9], popup ="Peek-a-boo!", icon =folium.Icon(color = "blue")))
+map = folium.Map(location = [43, 25], zoom_start = 6, tiles = "OpenStreetMap" ) #Current location when writing this
 
-map.save("Map test.html")
+fg = folium.FeatureGroup(name = "My Map")
+
+#Personalizing the map object with .add_child. Adding a marker.
+
+for lt, ln in zip(lat, lon):
+    fg.add_child(folium.Marker(location = [lt, ln], popup ="Peek-a-boo!", icon =folium.Icon(color = "green")))
+
+map.add_child(fg)
+
+
+map.save("Map test.html")   
