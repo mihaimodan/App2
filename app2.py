d498c7627b9d25997ed3a387bb49d6697f6e5425
mihaimodan <78228595+mihaimodan@users.noreply.github.com>
2021-02-23 22:38:59 +0200

    Updated the 2nd layer to show colors depending on the population density.

--- app2.py
+++ app2.py
@@ -15,7 +15,7 @@ def color_marker(elevation):
     else:
         return "red"
 
-map = folium.Map(location = [38, -99], zoom_start = 6, tiles = "OpenStreetMap" ) #Current location when writing this
+map = folium.Map(location = [38, -99], zoom_start = 6, tiles = "OpenStreetMap" ) #This gives us the initial location, zoom level, map background when opening the html.
 
 fg1 = folium.FeatureGroup(name = "My Map") #FeatureGroup acts as a layer.
 fg2 = folium.FeatureGroup(name = "Population Density Overlay")
@@ -25,8 +25,8 @@ for lt, ln, elv in zip(lat, lon, elev):
     fg1.add_child(folium.CircleMarker(location = [lt, ln], radius = 6, popup = "Elevation: %s m" %elv, fill_color = color_marker(elv), color = "grey", fill= True, fill_opacity = 0.7))
 
 
-#Adding a new layer of polygons for areas.
-fg2.add_child(folium.GeoJson(data=(open('world.json', 'r', encoding = "utf-8-sig").read())))
+#Adding a new layer of polygons with the data parameter. By adding the style_function parameter, we changed the color by population density.
+fg2.add_child(folium.GeoJson(data=open('world.json', 'r', encoding = "utf-8-sig").read(), style_function = lambda x: {'fillColor': 'green' if x['properties']['POP2005'] <= 5000000 else 'yellow' if 5000000 < x['properties']['POP2005'] < 15000000 else 'orange' if 15000000 < x['properties']['POP2005'] <=40000000 else 'red'}))
 
 map.add_child(fg1)
 map.add_child(fg2)
